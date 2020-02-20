105451cbfe656233d950dd662d9d70d7f08321f5
Dara-Shelli <meshyga@gmail.com>
2020-02-20 15:00:22 +0100

    Initial project add

---
+++ src/test/java/be/intecbrussel/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.springframe.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
